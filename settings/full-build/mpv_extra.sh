d8039c5ab50c56d15a3e8ef1a729c36d4e176911
okazunotri2013 <92250924+okazunori2013@users.noreply.github.com>
2022-10-16 18:50:38 +0900

    タイミング変更

--- settings/full-build/mpv_extra.sh
+++ settings/full-build/mpv_extra.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-_pre_configure(){
+_post_vcs(){
     do_patch "https://patch-diff.githubusercontent.com/raw/mpv-player/mpv/pull/10315.patch"
     do_patch "https://raw.githubusercontent.com/okazunori2013/patchs/main/mpv/add_--auto-window-resize_option.patch"
 }
