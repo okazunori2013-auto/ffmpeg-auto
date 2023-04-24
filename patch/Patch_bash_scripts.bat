affecc78a4764b451dd4ea4879c4106fcf969c08
okazunori2013 <92250924+okazunori2013@users.noreply.github.com>
2023-04-24 10:09:35 +0900

    Update Patch_bash_scripts.bat

--- patch/Patch_bash_scripts.bat
+++ patch/Patch_bash_scripts.bat
@@ -6,4 +6,4 @@ sed -i -r '/g (video^|audio) tools^|_check=\(xvc.pc^|sleep 5^|_check=\(libplaceb
 sed -i  '1i compact \/C \/S:media-autobuild_suite\/local64' media-autobuild_suite/media-autobuild_suite.bat
 
 rem for fix build
-sed -i  's!s:LIBPATH_lib(ass^|av(^|device^|filter))!s:LIBPATH_(lib)?(ffmpeg^|ass^|av^|archive^|bluray^|jpegxl(^|device^|filter))!g' media-autobuild_suite/build/media-suite_compile.sh
+sed -i 's!s:LIBPATH_lib(ass^|av(^|device^|filter))!s:LIBPATH_(lib)?(ffmpeg^|ass^|av^|archive^|bluray^|jpegxl(^|device^|filter))!g' media-autobuild_suite/build/media-suite_compile.sh
